1efa701c28bc2e6938cddf4fe27774e23de4e9c4
Ricardo Rocha Gonçalves <Ricardo.rg.neto@gmail.com>
2019-10-14 17:56:21 -0300

    Portifolio project ended

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,17 @@ class Blog(models.Model):
 	pub_date = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+
+	def __str__(self):
+		return self.title
+
+
+	def summary(self):
+		if len(self.body) > 100:
+			return self.body[:100] + "..."
+		else:
+			return self.body
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
